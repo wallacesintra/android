5b42b24f3960ea7e841f1bb19f2b0cfd7c239390
wallacesintra <wallvxwallace962@gmail.com>
2024-01-01 19:57:49 +0300

    arrays

---
+++ Arrays.kt
@@ -0,0 +1,16 @@
+/*
+* Array - way to group an arbitrary number of values
+*
+* An array has a fixed size.
+*
+* val arrayName = arrayOf<data type> (element1, element2, ...)
+*
+* access an element:
+*   arrayName[index]
+* */
+
+fun main(){
+    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
+    print(gasPlanets[0])
+
+}
